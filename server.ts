522ee28f492f6badb5905e58c12f4ff90213b1a8
Live Commerce <live-commerce@example.local>
2026-08-25 23:27:29 -0300

    Prepare project for deployment

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ if (!fs.existsSync(uploadDir)) {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT || 3000);
 
   app.use(cors());
   app.use(express.json());
